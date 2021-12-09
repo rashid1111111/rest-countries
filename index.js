7a3d34fe7e67b32b6fb08df1d3ede313cb7e2055
rashid1111111 <rashidbek0309@gmail.com>
2021-12-09 09:43:35 +0500

    Rewiew

--- index.js
+++ index.js
@@ -1,7 +1,4 @@
 const elDarkMode = document.querySelector(".header-dark-mode");
 elDarkMode.addEventListener('click', function(){
-    document.body.classList.add('dark-mode');
-});
-elDarkMode.addEventListener('dblclick', function(){
-    document.body.classList.remove('dark-mode');
+    document.body.classList.toggle('dark-mode');
 });
